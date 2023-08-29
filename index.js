fe99fb86ab52d92340ea8c6027294591b4c46de3
Shivansh Dutta <109021679+EssDee1@users.noreply.github.com>
2023-08-30 00:36:56 +0545

    second commit

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
